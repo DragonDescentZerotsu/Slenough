b58f3708c03fa144113f34f2f7eae407626cbbab
kirianozan <lingchunsusan@gmail.com>
2026-05-16 11:00:42 -0500

    Initial Commit

---
+++ SleepKitProbe/SleepKitProbeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepKitProbeApp.swift
+//  SleepKitProbe
+//
+//  Created by Kiria Nozan on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SleepKitProbeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
